dae28a670134817fcc7e7b6925a9fc584c5daaeb
Bryant CH <bchihola@gmail.com>
2023-06-11 01:38:35 +0700

    integrate db

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ app = Flask(__name__)
 CORS(app)
 
 # MySQL configurations
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'your_username')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'your_password')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'your_database_name')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '35.209.111.117')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'rakit-api-db')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'c23ps001')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'suarakita')
 
 mysql = MySQL(app)
 
@@ -100,13 +100,118 @@ def add_user():
 
 @app.route('/training', methods=['POST'])
 def train():
-    # Your training code here
-    return jsonify({'message': 'Training endpoint'})
+    getGambar1 = request.files['image1']
+    getGambar2 = request.files['image2']    
+    getGambar1.save(getGambar1.filename)
+    getGambar2.save(getGambar2.filename)
+    idUser = request.form.get("idUser")
+
+    model = Model(inputs=[imgA, imgB], outputs=outputs)
+    model.load_weights("./transfer.h5")
+    
+    credentials_path = r"nyobaaja-973da4b3851c.json"
+    client = storage.Client.from_service_account_json(credentials_path)
+    bucket_name = 'upload_foto'
+    bucket = client.get_bucket(bucket_name)
+
+
+    gambar1=preprocess_image(np.array(Image.open(getGambar1)))[0]#Gambar Lurus
+    gambar2=preprocess_image(np.array(Image.open(getGambar2)))[0]#Gambar Samping
+    gambar3=gambar2[:,::-1]
+
+    pic1=preprocess_image(np.array(tf.keras.preprocessing.image.load_img("./lawan_1.png")))[0]
+    pic2=preprocess_image(np.array(tf.keras.preprocessing.image.load_img("./lawan_2.png")))[0]
+    pic3=preprocess_image(np.array(tf.keras.preprocessing.image.load_img("./lawan_3.png")))[0]
+    
+    
+    gambar1Name = str(uuid.uuid4())+"_"+getGambar1.filename
+    gambar2Name = str(uuid.uuid4())+"_"+getGambar2.filename
+
+    
+    blob1 = bucket.blob('fotoselfie/{}'.format(gambar1Name))
+    blob2 = bucket.blob('fotoselfie/{}'.format(gambar2Name))
+    blob1.upload_from_filename(getGambar1.filename)
+    blob2.upload_from_filename(getGambar2.filename)
+    linkFoto1 = blob1.public_url
+    linkFoto2 = blob2.public_url
+    
+    os.remove(getGambar1.filename)
+    os.remove(getGambar2.filename)
+    
+    #Array
+    arr_gambar=np.array([gambar1,gambar2,gambar3])
+    arr_pic=np.array([pic1,pic2,pic3])
+    print(arr_gambar.shape, arr_pic.shape)
+    gab=np.append(arr_gambar,arr_pic,axis=0)
+    gablabel=np.append(np.ones(3),np.zeros(3))
+    sampled_indices = np.random.choice(gab.shape[0], size=(3,), replace=False)
+    sampled_array = gab[sampled_indices]
+    sampled_label = gablabel[sampled_indices]
+    unsampled_array=gab[~np.isin(np.arange(6), sampled_indices)]
+    unsampled_label=gablabel[~np.isin(np.arange(6), sampled_indices)]
+    image_data=[]
+    label_data=[]
+    for i in range(3):
+        for j in range(3):
+            image_data.append([sampled_array[i],unsampled_array[j]])
+            label_data.append(sampled_label[i]*unsampled_label[j])
+    label_data=np.array(label_data)
+    image_data=np.array(image_data)
+    
+    #Building Model
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])  
+    print(image_data.shape, label_data.shape)
+    history = model.fit([image_data[:, 0], image_data[:, 1]], label_data[:],validation_split=0.1,batch_size=64,epochs=50)
+    #Saving Model
+    h5 = str(uuid.uuid4())+"_"+idUser+"_train.h5"
+    
+    
+    model.save_weights("./"+h5)
+    h5Blob = bucket.blob('thomasandfriend/{}'.format(h5))
+    h5Blob.upload_from_filename("./"+h5)
+    linkModel = h5Blob.public_url
+
+    
+    os.remove("./"+h5)
+    
+    return json.dumps({"linkFoto1": linkFoto1, "linkFoto2": linkFoto2, "linkmodel": linkModel})
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Your prediction code here
-    return jsonify({'message': 'Prediction endpoint'})
+    getGambar1 = request.files['Gambar']#Files Gambar Aldo
+    getGambar1=preprocess_image(np.array(Image.open(getGambar1)))[0]
+
+    getGambar2 = request.form.get('linkFoto2')
+    getGambar2 = requests.get(getGambar2)
+    getModel = request.form.get('linkModel')
+    modelFileName = getModel.split('/')[-1]
+    getModel = requests.get(getModel)
+    
+    with open('./'+modelFileName, 'wb') as f:
+        f.write(getModel.content)
+    
+    #Building Model
+    validasi= getGambar1
+    anchor= np.array(Image.open(BytesIO(getGambar2.content)))
+    new_model = Model(inputs=[imgA, imgB], outputs=outputs)
+    new_model.load_weights("./"+modelFileName)
+
+    #processing
+    validasi=preprocess_image(validasi)[0]
+    anchor=preprocess_image(anchor)[0]
+    print(validasi.shape,anchor.shape)
+
+    #Prediction 
+    Hasil=new_model.predict([np.array([validasi]),np.array([anchor])])[0][0]
+    print(type(Hasil))
+    
+    os.remove("./"+modelFileName)
+
+    #Threshold
+    if Hasil>0.4:
+        return json.dumps({'Predict':True})
+    else:
+        return json.dumps({'Predict':False})
 
 if __name__ == '__main__':
     app.run(debug=True)
